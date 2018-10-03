9b7e8dea5e88127cdb1fd75e294651d7fb866fda
Lucas Werner <lwerner27@gmail.com>
2018-10-02 22:36:14 -0500

    basic functions working

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,47 @@ import React, { Component } from 'react';
 import FrameworkContainer from "./components/frameworkContainer/FrameworkContainer.js"
 
 class App extends Component {
+
+  state = {
+    frameworks: []
+  }
+
+  componentDidMount() {
+    let url = "https://api.github.com/repos/"
+    let repoInfo = [
+      "angular/angular",
+      "facebook/react",
+      "emberjs/ember.js",
+      "vuejs/vue"
+    ]
+
+    repoInfo.forEach((repo) => {
+      fetch(`${url}${repo}`)
+      .then((res) => res.json())
+      .then((data) => {
+        let frameworks = this.state.frameworks
+        let frameworkData = {
+          name: data.name,
+          stars: data.stargazers_count,
+          forks: data.forks,
+          issues: data.open_issues,
+          issuesLink: `https://github.com/${repo}/issues`
+        }
+        frameworks.push(frameworkData)
+        this.setState({frameworks: frameworks})
+        
+      })
+    })
+  }
   
   render() {
+    
     return (
       <div className="container">
-        <div className="row">
-          <FrameworkContainer />
-          <FrameworkContainer />
-          <FrameworkContainer />
-          <FrameworkContainer />
+        <div className="row center">
+          { this.state.frameworks.map((framework) => {
+            return <FrameworkContainer name={framework.name} stars={framework.stars} forks={framework.forks} issues={framework.issues} />
+          })}
         </div>
       </div>
     );
--- client/src/components/frameworkContainer/FrameworkContainer.js
+++ client/src/components/frameworkContainer/FrameworkContainer.js
@@ -4,11 +4,11 @@ class FrameworkContainer extends React.Component {
     render() {
         return (
             <div className="col s12 m3">
-                <h5>React</h5>
+                <h5>{this.props.name}</h5>
                 <ul>
-                    <li><strong>Stars: </strong>25</li>
-                    <li><strong>Forks: </strong>15</li>
-                    <li><strong>Issues: </strong><a href="#"></a></li>
+                    <li><strong>Stars: </strong>{this.props.stars}</li>
+                    <li><strong>Forks: </strong>{this.props.forks}</li>
+                    <li><strong>Issues: </strong>{this.props.issues}</li>
                 </ul>
 
             </div>
